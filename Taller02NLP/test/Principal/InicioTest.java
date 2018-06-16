3dfa2be3ca1dd233b8bc768dac3b56168d4ff404
djingeniero <38969088+djingeniero@users.noreply.github.com>
2018-06-16 09:30:40 -0500

    pruebas parte 3

--- Taller02NLP/test/Principal/InicioTest.java
+++ Taller02NLP/test/Principal/InicioTest.java
@@ -87,7 +87,7 @@ public class InicioTest {
         String[] args = null;
         Inicio.main(args);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
     
 }
